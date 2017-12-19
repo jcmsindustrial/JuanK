508df5a294942705e3e33627d2665eec8c6d6eb4
Duoc UC <Duoc UC@DESKTOP-81TE2J2>
2017-12-19 19:53:13 -0300

    Agrego MVC y una vista de mierda

--- src/proyectomedina_quila/ProyectoMedina_Quila.java
+++ src/proyectomedina_quila/ProyectoMedina_Quila.java
@@ -1,6 +1,11 @@
 
 package proyectomedina_quila;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Duoc UC
@@ -10,8 +15,17 @@ public class ProyectoMedina_Quila {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (InstantiationException ex) {
+            Logger.getLogger(ProyectoMedina_Quila.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(ProyectoMedina_Quila.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(ProyectoMedina_Quila.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     
